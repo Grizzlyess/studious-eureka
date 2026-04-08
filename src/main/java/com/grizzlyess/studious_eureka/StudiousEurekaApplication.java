3f02939b69a9ee8f4862e58bf566532499c21b8b
Clara <mariaclarab316@gmail.com>
2026-04-08 10:52:13 -0300

    teste

---
+++ src/main/java/com/grizzlyess/studious_eureka/StudiousEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.grizzlyess.studious_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiousEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiousEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grizzlyess/studious_eureka/StudiousEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grizzlyess.studious_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiousEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
