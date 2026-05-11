65afb0c7ad5beda039c85b1f5802920a0b92a8c0
Clara <mariaclarab316@gmail.com>
2026-05-11 14:30:24 -0300

    testes

--- src/test/java/com/grizzlyess/studious_eureka/StudiousEurekaApplicationTests.java
+++ src/test/java/com/grizzlyess/studious_eureka/StudiousEurekaApplicationTests.java
@@ -7,12 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class StudiousEurekaApplicationTests {
-
-
-
 
 	@Autowired
 	private WebTestClient webTestClient;
